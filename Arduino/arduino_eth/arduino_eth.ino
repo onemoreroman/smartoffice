e49a604bbc3580e4ca23c774798cd9896898ed9a
Roman Tokmashev <rtokmashev@openprovider.nl>
2021-02-13 15:40:30 +0700

    Arduino code updated

--- Arduino/arduino_eth/arduino_eth.ino
+++ Arduino/arduino_eth/arduino_eth.ino
@@ -12,14 +12,12 @@ DHT dht(7, DHT11);
 
 // Ethernet
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };  // MAC address
-EthernetClient eth_client;
+EthernetClient eth;
 
 // Server
-String auth_user = SECRET_SERVER_USER;
-String auth_password = SECRET_SERVER_PASSWORD;
-HttpClient http_client = HttpClient(eth_client, SECRET_SERVER_HOST, SECRET_SERVER_PORT);
+HttpClient http = HttpClient(eth, SECRET_SERVER_HOST, SECRET_SERVER_PORT);
 
-float read_avg_vol(int analog_pin, int N) {
+float readAvgVoltage(int analog_pin, int N) {
   float v; int i;
   v = analogRead(analog_pin);
   for (i=2; i<=N; i++) {
@@ -29,7 +27,7 @@ float read_avg_vol(int analog_pin, int N) {
   return 5*(v/1023);
 }
 
-float volt_to_temp(float v, float R0, float B, float T0) {
+float convertVoltageToTemp(float v, float R0, float B, float T0) {
   float T, RtoR0;
   RtoR0 = v/(5-v);
   T = log(RtoR0)/B;
@@ -39,17 +37,30 @@ float volt_to_temp(float v, float R0, float B, float T0) {
   return T;
 }
 
-void send_http_request(String json) {
-  http_client.beginRequest();
-  http_client.post("/office/upload_data");
-  http_client.sendHeader("Content-Type", "application/json");
-  //http_client.sendHeader("Content-Length", 0);//postdata.length());
-  http_client.sendBasicAuth(auth_user, auth_password);
-  http_client.beginBody();
-  http_client.print(json);
-  http_client.print("\r\n");  
-  delay(500); // avoid "Broken pipe [core/writer.c line 306] during POST /office/upload_data"
-  http_client.endRequest();
+void postJSON(String& json) {
+  http.beginRequest();
+  http.post("/office/upload_data");
+  http.sendHeader("Content-Type", "application/json");
+  http.sendHeader("Content-Length", json.length());
+  http.sendBasicAuth(SECRET_SERVER_USER, SECRET_SERVER_PASSWORD);
+  http.beginBody();
+  http.print(json);
+  http.print("\r\n");
+  delay(200); // avoid "Broken pipe [core/writer.c line 306] during POST /office/upload_data"
+//  Serial.println(http.readString()); // Print response for debugging
+  http.endRequest();
+  http.stop();
+  json = "";
+}
+
+void jsonifySensorData(String sensorName, float sensorValue, String& json)
+{
+  StaticJsonDocument<100> doc;
+
+  doc["sensor_name"] = sensorName;
+  doc["sensor_value"] = sensorValue;
+
+  serializeJson(doc, json);
 }
 
 void setup() {
@@ -72,61 +83,41 @@ void setup() {
 void loop() {
   float v;
   String json;
-  StaticJsonDocument<256> doc;
 
-  // Ask sensor 1
-  v = dht.readHumidity();
-  doc["sensor_name"] = "dht11_humi";
-  doc["sensor_value"] = v;
-  serializeJson(doc, json);
-  //serializeJson(doc, Serial);
-  send_http_request(json);
-  doc.clear()
+//  // Sensor 1: DHT11, turned out to be unstable :(
+//  v = dht.readHumidity();
+//  jsonifySensorData("dht11_humi", v, json);
+//  postJSON(json);
+//  delay(2000);
+
+  // Sensor 2: NTC10k 
+  v = readAvgVoltage(A0, 5);
+  v = convertVoltageToTemp(v, 8450, 3500, 25);
+  jsonifySensorData("ntc10k_temp2", v, json);
+  postJSON(json);
   delay(2000);
 
-//  // Ask sensor 2
-//  v = read_avg_vol(A0, 5);
-//  v = volt_to_temp(v, 8450, 3500, 25);
-//  Serial.println(v);
-//  doc["sensor_name"] = "ntc10k_temp2";
-//  doc["sensor_value"] = v;
-//  //serializeJson(doc, json);
-//  serializeJson(doc, Serial);
-//  //send_http_request(json);
-//  delay(2000);
-//
-//  // Ask sensor 3
-//  v = read_avg_vol(A1, 5);
-//  Serial.println(v);
-//  v = volt_to_temp(v, 8760, 3500, 25);
-//  Serial.println(v);
-//  postdata = prep_post_data("ntc10k_temp", v);
-//  Serial.println(postdata);
-//  delay(500);
-//  //send_http_request(http_client, postdata);
-//  delay(2000);
-//  
-//  // Ask sensor 4
-//  v = read_avg_vol(A3, 5);
-//  Serial.println(v);
-//  v = volt_to_temp(v, 8530, 3500, 25);
-//  Serial.println(v);
-//  postdata = prep_post_data("ntc10k_temp1", v);
-//  Serial.println(postdata);
-//  delay(500);
-//  //send_http_request(http_client, postdata);
-//  delay(2000);
-//  
-//  // Ask sensor 5
-//  if(ccs.available()){
-//    if(!ccs.readData()){
-//      v = ccs.getTVOC();
-//      Serial.println(v);
-//      postdata = prep_post_data("ccs811_tvoc", v);
-//      Serial.println(postdata);
-//      delay(500);
-//      //send_http_request(http_client, postdata);
-//    }
-//  }  
-//  delay(10000);
+  // Sensor 3: NTC10k
+  v = readAvgVoltage(A1, 5);
+  v = convertVoltageToTemp(v, 8760, 3500, 25);
+  jsonifySensorData("ntc10k_temp", v, json);
+  postJSON(json);
+  delay(2000);
+  
+  // Sensor 4: NTC10k
+  v = readAvgVoltage(A3, 5);
+  v = convertVoltageToTemp(v, 8530, 3500, 25);
+  jsonifySensorData("ntc10k_temp1", v, json);
+  postJSON(json);
+  delay(2000);
+  
+  // Sensor 5: CCS811
+  if(ccs.available()){
+    if(!ccs.readData()){
+      v = ccs.getTVOC();
+      jsonifySensorData("ccs811_tvoc", v, json);
+      postJSON(json);
+    }
+  }  
+  delay(10000);
 }
--- Arduino/arduino_eth/secrets_template.h
+++ Arduino/arduino_eth/secrets_template.h
@@ -1,4 +1,4 @@
-String SECRET_SERVER_USER = "user";
-String SECRET_SERVER_PASSWORD = "pass";
-String SECRET_SERVER_HOST = "user_domain.com";
+char SECRET_SERVER_USER[] = "user";
+char SECRET_SERVER_PASSWORD[] = "pass";
+char SECRET_SERVER_HOST[] = "user_domain.com";
 int SECRET_SERVER_PORT = 8000;
